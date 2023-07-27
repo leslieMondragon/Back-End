44a5fc49b58d94df1eaa5c3d9c281c0598bdc4a7
leslieMondragon <iscmond@gmail.com>
2023-07-27 11:38:29 -0600

    Cuarta Practica Integradora

---
+++ DesafioPractIntegradora4/src/public/js/register.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("registerForm");
+
+form.addEventListener('submit', event => {
+
+    event.preventDefault();
+
+    const data = new FormData(form);
+    const object = {};
+
+    data.forEach((value, key) => {
+        object[key] = value;
+    })
+    
+    fetch('/api/session/register', {
+        method: 'POST',
+        body: JSON.stringify(object),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+    // .then(result => result.json()).then(json => console.log(json))
+    .then(
+        result => result.json()).then(json => {
+            if (json.status == 'Ok') {
+                Swal.fire({
+                    icon: '',
+                    title: 'Cuenta creada exitosamente'
+                })
+                setTimeout(function() {location.replace('/login');}, 900);
+            } else {
+                Swal.fire({
+                    icon: '',
+                    title: "Cuenta no creada",
+                    text: json.error || "Ya existe una cuenta con ese email"
+                })
+            }
+        }
+    )
+})
